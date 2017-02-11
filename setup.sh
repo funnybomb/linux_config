43fe52167a312f4aa4c7e5863d6fa229d51679d2
funnybomb <feicaiyi@gmail.com>
2017-02-11 20:00:36 +0800

    sshd config 01

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ cp ./bashrc ~/.bashrc
 cp ./bash_profile ~/.bash_profile
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-cp -r ./ssh/* ~/.ssh/
+cp -r ./ssh/* ~/.ssh/ #config pubs authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
 
