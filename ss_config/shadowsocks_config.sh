0290617c6c98592e7e9c9a0748ff418fa1764e6c
funnybomb <feicaiyi@gmail.com>
2017-02-12 11:03:39 +0800

    proxychains

--- ss_config/shadowsocks_config.sh
+++ ss_config/shadowsocks_config.sh
@@ -1,10 +1,12 @@
-#sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
-#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
-#sudo apt-get update
-#sudo apt-get install google-chrome-stable
-## /usr/bin/google-chrome-stable find chrome and lock it
-#sudo apt-get install supervisor 
-#sudo apt install shadowsocks
+sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+# /usr/bin/google-chrome-stable find chrome and lock it
+sudo apt-get install supervisor 
+sudo apt install shadowsocks
+
+#setup config
 sudo cp ./ss_bwg_2017.json /etc/supervisor/conf.d/
 sudo cp ./ss_supervisor.conf /etc/supervisor/conf.d/
 sudo cp /usr/bin/sslocal /bin/
@@ -12,3 +14,8 @@ sudo service supervisor restart
 sudo sed -i '$i\service supervisor start' /etc/rc.local
 ps -ef | grep ssloca
 ps -ef | grep supervisor
+
+# configure polipo for shell
+sudo apt-get install proxychains
+sudo cp ./proxychains.conf /etc/proxychains.conf
+
